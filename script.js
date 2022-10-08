f805cf172f22c0bd5819f66f2497944575bd3933
Sirajum Mahdi <sirajummahdi@gmail.com>
2022-10-08 19:11:49 +0600

    Update script.js

--- script.js
+++ script.js
@@ -72,7 +72,7 @@ btnHold.addEventListener('click', function () {
       scores[activePlayer];
 
     // 2. Check if player's score is >= 100
-    if (scores[activePlayer] >= 20) {
+    if (scores[activePlayer] >= 100) {
       playing = false;
       diceEl.classList.add('hidden');
 
